85ebeb193b10c022bfb49346199a4fd41ac13173
shiv.prakash <shiv.prakash@cgi.com>
2019-07-29 14:20:06 +0530

    aware interface task 4 commit

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,9 +3,9 @@ e
 5src/main/java/com/stackroute/spring/domain/Actor.java,1/2/128c13311135d31c1cd9272f171d91aaf348eea8
 e
 5src/main/java/com/stackroute/spring/domain/Movie.java,d/9/d98ece54da8d667c1b42f72092242ff65dcf3ada
-G
-src/resources/beans.xml,4/6/468fa779d80990438c69fe182cfb03a0d5255192
 7
 pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
-]
--src/main/java/com/stackroute/spring/Main.java,3/d/3d5121ec36b79538ee142ce017a1a81c16bbd335
+\
+,src/main/java/com/stackroute/spring/App.java,c/6/c63766dae0b1b0b424b9edb9f3d8f499a9d36b0f
+G
+src/resources/beans.xml,4/6/468fa779d80990438c69fe182cfb03a0d5255192
--- src/main/java/com/stackroute/spring/App.java
+++ src/main/java/com/stackroute/spring/App.java
@@ -1,4 +1,9 @@
 package com.stackroute.spring;
+
+/**
+ * Hello world!
+ *
+ */
 import com.stackroute.spring.domain.Movie;
 import com.stackroute.spring.domain.Actor;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
@@ -7,22 +12,12 @@ import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
-
-/**
- * Hello world!
- *
- */
-public class Main
+public class App 
 {
     public static void main( String[] args )
     {
         ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
         Movie movie =(Movie) context.getBean("movie");
         System.out.println(movie);
-//        XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
-//        Movie movie1=(Movie) factory.getBean("movie1");
-//        System.out.println(movie1);
-
     }
 }
-
