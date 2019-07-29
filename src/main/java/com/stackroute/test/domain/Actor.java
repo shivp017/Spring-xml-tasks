a1dfbef72a907fe1e0cf3d7b9d84069c819d0a94
shiv.prakash <shiv.prakash@cgi.com>
2019-07-29 12:59:35 +0530

    Assignment 1st commit

---
+++ src/main/java/com/stackroute/test/domain/Actor.java
@@ -0,0 +1,51 @@
+package com.stackroute.test.domain;
+
+public class Actor {
+    private int age;
+    private String name;
+    private String gender;
+
+
+    public Actor() {
+    }
+
+
+    public Actor(int age, String name, String gender) {
+        this.age = age;
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Actor{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/stackroute/test/domain/Movie.java
@@ -0,0 +1,28 @@
+package com.stackroute.test.domain;
+
+public class Movie {
+    private Actor actor;
+
+    public Actor getActor() {
+        return actor;
+    }
+
+    public void setActor(Actor actor) {
+        this.actor = actor;
+    }
+
+    public Movie(Actor actor) {
+
+        this.actor = actor;
+    }
+    public Movie() {
+    }
+
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "actor=" + actor +
+                '}';
+    }
+}
